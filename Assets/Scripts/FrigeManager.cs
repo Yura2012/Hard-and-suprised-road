d86a69d0433d9b36f677c3b9346eeae59b1a8455
Mark Lekhnovskyi <lekhnovskyimark@gmail.com>
2025-01-29 21:13:52 +0200

    Zrobyv Fridge Menu

--- Assets/Scripts/FrigeManager.cs
+++ Assets/Scripts/FrigeManager.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class FridgeManager : MonoBehaviour
 {
-    public int AppleMax, BananaMax;
-    public int AppleAmount, BananaAmount;
-    public Text AppleText, BananaText;
-    public Slider HP, Thirst, Hunger;
+    public int AppleMax, BananaMax, CakeMax, WaterMax;
+    public int AppleAmount, BananaAmount, CakeAmount, WaterAmount;
+    public Text AppleText, BananaText, CakeText, WaterText;
+    public Slider HP, Thirst, Hunger, Fatigue;
     public GameObject FridgeUiObj;
     public FirstPersonController FPScontroller;
     public GameObject FrigeCanvas;
@@ -20,10 +20,13 @@ public class FridgeManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
 
         Hunger.value -= 1 * Time.deltaTime;
-        Hunger.value -= 2 * Time.deltaTime;
+        Thirst.value -= 2 * Time.deltaTime;
+        Fatigue.value -= 1 * Time.deltaTime;
         // Оновлення тексту для відображення кількості яблук
         AppleText.text = "Apple: " + AppleAmount.ToString() + " / " + AppleMax.ToString();
-        AppleText.text = "Banana: " + BananaAmount.ToString() + " / " + BananaMax.ToString();
+        BananaText.text = "Banana: " + BananaAmount.ToString() + " / " + BananaMax.ToString();
+        CakeText.text = "Cake: " + CakeAmount.ToString() + " / " + CakeMax.ToString();
+        WaterText.text = "Bottles of water: " + WaterAmount.ToString() + " / " + WaterMax.ToString();
 
         // Зменшення HP, якщо значення Thirst або Hunger <= 0
         if (Thirst.value <= 0)
@@ -36,8 +39,13 @@ public class FridgeManager : MonoBehaviour
             HP.value -= 1 * Time.deltaTime;
         }
 
+        if (Fatigue.value <= 0)
+        {
+            HP.value -= 3 * Time.deltaTime;
+        }
+
         // Відновлення HP, якщо Hunger і Thirst > 50
-        if (Hunger.value > 50 && Thirst.value > 50)
+        if (Hunger.value > 50 && Thirst.value > 50 && Fatigue.value > 50)
         {
             HP.value += 1 * Time.deltaTime;
         }
@@ -60,6 +68,26 @@ public class FridgeManager : MonoBehaviour
         {
             BananaAmount -= 1;
             Hunger.value += 8; // Наприклад, споживання яблука відновлює голод
+        }
+    }
+
+    public void EatCake()
+    {
+        // Перевірка, чи є яблука в холодильнику
+        if (CakeAmount > 0)
+        {
+            CakeAmount -= 1;
+            Hunger.value += 12; // Наприклад, споживання яблука відновлює голод
+        }
+    }
+
+    public void DrinkWater()
+    {
+        // Перевірка, чи є яблука в холодильнику
+        if (WaterAmount > 0)
+        {
+            WaterAmount -= 1;
+            Thirst.value += 10; // Наприклад, споживання яблука відновлює голод
         }
     }
 
